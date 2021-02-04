4d080da09572fe6cec05ae0d1f1dae5c4a6fe777
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-02-04 13:46:05 +0530

    complete18vidio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Routes,RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NgforComponent } from './ngfor/ngfor.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgifComponent } from './ngif/ngif.component';
 import { NgswitchComponent } from './ngswitch/ngswitch.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NgforComponent,
     NgifComponent,
-    NgswitchComponent
+    NgswitchComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ngfor/ngfor.component.ts
+++ src/app/ngfor/ngfor.component.ts
@@ -1,39 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
-interface Country {
-  name: string;
-  flag: string;
-  area: number;
-  population: number;
-}
-const COUNTRIES: Country[] = [
-  {
-    name: 'Russia',
-    flag: 'f/f3/Flag_of_Russia.svg',
-    area: 17075200,
-    population: 146989754
-  },
-  {
-    name: 'Canada',
-    flag: 'c/cf/Flag_of_Canada.svg',
-    area: 9976140,
-    population: 36624199
-  },
-  {
-    name: 'United States',
-    flag: 'a/a4/Flag_of_the_United_States.svg',
-    area: 9629091,
-    population: 324459463
-  },
-  {
-    name: 'China',
-    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    area: 9596960,
-    population: 1409517397
-  }
-];
-
 @Component({
   selector: 'app-ngfor',
   templateUrl: './ngfor.component.html',
@@ -44,12 +10,35 @@ export class NgforComponent implements OnInit {
 
   constructor() { }
 
+  products = [
+    {
+      'image': 'https://images.unsplash.com/photo-1462450806522-59ae61ea99fa',
+      'name': 'Mobile',
+      'id': 'P01',
+      'price': '20.60'
+    },
+    {
+      'image': 'https://images.unsplash.com/photo-1425321505542-00715f6a9c28',
+      'name': 'Leptop',
+      'id': 'P02',
+      'price': '23.60'
+    }
+  ];
+  users = [];
+  onCreateUser(uname) {
+    this.users.push({
+      name: uname.value
+    })
+  }
+  onRemoveUser(item){
+    console.log(item);
+    this.users.splice(item, 1)
+  }
+
 
   ngOnInit(): void {
   }
 
-
-  countries = COUNTRIES;
 
 
 
