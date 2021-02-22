372df82f2d442ea42b84787f88fc386a02c16f9d
Shahnawaz Alam <shahnawazshafiss9@gmail.com>
2021-02-22 20:10:01 +0530

    31 vedio done

---
+++ src/app/appService/designutility.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DesignutilityService {
+
+  constructor(private http: HttpClient) { }
+  onSubmitAlert() {
+    alert('Alert, contact you soon shortly');
+  }
+  product(): Observable<any>{
+    return this.http.get('https://jsonplaceholder.typicode.com/users/1/posts');
+  }
+
+}
